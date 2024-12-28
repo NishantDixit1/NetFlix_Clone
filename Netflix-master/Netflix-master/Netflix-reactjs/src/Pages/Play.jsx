992811cd9f2e0218acf429a5deb139d9e06f2fe8
Nishant Dixit <125440575+NishantDixit1@users.noreply.github.com>
2024-12-28 22:41:28 +0530

    Update Play.jsx

--- Netflix-master/Netflix-master/Netflix-reactjs/src/Pages/Play.jsx
+++ Netflix-master/Netflix-master/Netflix-reactjs/src/Pages/Play.jsx
@@ -346,21 +346,22 @@ function Play() {
                   </button>
                 </div>
               </div>
-              <img
-                src={
-                  movieDetails.poster_path &&
-                  `${
-                    imageUrl +
-                    (window.innerWidth > 1024
-                      ? movieDetails.backdrop_path
-                        ? movieDetails.backdrop_path
-                        : "https://i.ytimg.com/vi/Mwf--eGs05U/maxresdefault.jpg"
-                      : movieDetails.poster_path)
-                  }`
-                }
-                className="w-40 rounded-sm lg:w-[45rem] ml-4 lg:ml-0"
-                alt=<img src="https://i.ytimg.com/vi/Mwf--eGs05U/maxresdefault.jpg" />
-              />
+             <img
+  src={
+    movieDetails.poster_path &&
+    `${
+      imageUrl +
+      (window.innerWidth > 1024
+        ? movieDetails.backdrop_path
+          ? movieDetails.backdrop_path
+          : "https://i.ytimg.com/vi/Mwf--eGs05U/maxresdefault.jpg"
+        : movieDetails.poster_path)
+    }`
+  }
+  className="w-40 rounded-sm lg:w-[45rem] ml-4 lg:ml-0"
+  alt="Thumbnail for a video"
+/>
+
             </div>
           </section>
 
